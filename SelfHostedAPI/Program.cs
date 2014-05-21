142ad4621ea4388f7259970f9dabdecb467f2a8d
Matt Sell <matt.sell@improvingenterprises.com>
2014-05-21 11:13:26 -0500

    initial commit

---
+++ SelfHostedAPI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using Microsoft.Owin.Hosting;
+
+namespace SelfHostedAPI
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         string baseAddress = "http://localhost:9000/";
+
+         // Start OWIN host 
+         using (WebApp.Start<Startup>(url: baseAddress))
+         {
+            // Create HttpCient and make a request to api/values 
+            HttpClient client = new HttpClient();
+
+            var response = client.GetAsync(baseAddress + "api/upload").Result;
+
+            Console.ReadLine();
+         }
+
+      }
+   }
+}
---
+++ SelfHostedAPI/UploadController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SelfHostedAPI
+{
+   public class UploadController : ApiController
+   {
+      public IHttpActionResult Get()
+      {
+         return Ok("test");
+      }
+
+      static readonly string ServerUploadFolder = @"c:\temp\uploads";
+
+      [HttpPut]
+      public async Task<FileResult> Put()
+      {
+         // Verify that this is an HTML Form file upload request
+         if (!Request.Content.IsMimeMultipartContent("form-data"))
+         {
+            throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.UnsupportedMediaType));
+         }
+
+         // Create a stream provider for setting up output streams
+         CustomFormDataStreamProvider streamProvider = new CustomFormDataStreamProvider(ServerUploadFolder);
+
+         // Read the MIME multipart asynchronously content using the stream provider we just created.
+         await Request.Content.ReadAsMultipartAsync(streamProvider);
+
+         // Create response
+         return new FileResult
+         {
+            FileNames = streamProvider.FileData.Select(entry => entry.LocalFileName),
+            Submitter = streamProvider.FormData["submitter"]
+         };
+      }
+   }
+
+   public class CustomFormDataStreamProvider :MultipartFormDataStreamProvider
+   {
+      public CustomFormDataStreamProvider(string rootPath) : base(rootPath)
+      {
+      }
+
+      public override string GetLocalFileName(System.Net.Http.Headers.HttpContentHeaders headers)
+      {
+         if (headers == null)
+         {
+            throw new NullReferenceException("headers cant be null");
+         }
+         return headers.ContentDisposition.FileName.Replace("\"","");
+      }
+   }
+}
